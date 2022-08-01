3989ad55ad29c62d8ed2ce78b7cc70a633f73658
Kevin Bowen <kevin.bowen@gmail.com>
2022-07-31 20:51:54 -0700

    Implement routers to simplify URLs

--- posts/urls.py
+++ posts/urls.py
@@ -1,11 +1,9 @@
-from django.urls import path
+from rest_framework.routers import SimpleRouter
 
-from .views import PostList, PostDetail, UserList, UserDetail
+from .views import PostViewSet, UserViewSet
 
+router = SimpleRouter()
+router.register("users", UserViewSet, basename="users")
+router.register("", PostViewSet, basename="posts")
 
-urlpatterns = [
-    path("users/", UserList.as_view()),
-    path("users/<int:pk>/", UserDetail.as_view()),
-    path("", PostList.as_view(), name="post_list"),
-    path("<int:pk>/", PostDetail.as_view(), name="post_detail"),
-]
+urlpatterns = router.urls
