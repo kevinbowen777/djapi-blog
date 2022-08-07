85dbd63e74e4c98d8b07bb2337374c3fded72008
Kevin Bowen <kevin.bowen@gmail.com>
2022-08-07 12:30:04 -0700

    Add str method, age & bio fields to CustomUser model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,22 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
-from .models import CustomUser
+
+User = get_user_model()
 
 
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
-    model = CustomUser
+    model = User
     list_display = [
         "email",
         "username",
-        "name",
         "is_staff",
     ]
-    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("name",)}),)
-    add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("name",)}),)
+    fieldsets = UserAdmin.fieldsets
+    fieldsets[1][1]["fields"] = fieldsets[1][1]["fields"] + (
+        "age",
+        "bio",
+    )
 
 
-admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(User, CustomUserAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -3,4 +3,11 @@ from django.db import models
 
 
 class CustomUser(AbstractUser):
-    name = models.CharField(null=True, blank=True, max_length=100)
+    name = models.CharField(
+        "Name of User", null=True, blank=True, max_length=100
+    )
+    age = models.PositiveIntegerField(null=True, blank=True)
+    bio = models.TextField("Bio", blank=True)
+
+    def __str__(self):
+        return self.username
