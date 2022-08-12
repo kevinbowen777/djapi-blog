a981c31e0bf01e6369a08401251aa1745b5e59cf
Kevin Bowen <kevin.bowen@gmail.com>
2022-08-12 13:20:42 -0700

    Add PostListView. Resolves #10

--- posts/views.py
+++ posts/views.py
@@ -1,19 +1,36 @@
 from django.contrib.auth import get_user_model
-from rest_framework import viewsets
-from rest_framework.permissions import IsAdminUser
+from django.views.generic import ListView
+from rest_framework import generics
 
 from .models import Post
 from .permissions import IsAuthorOrReadOnly
 from .serializers import PostSerializer, UserSerializer
 
 
-class PostViewSet(viewsets.ModelViewSet):
+class PostList(generics.ListCreateAPIView):
     permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
-class UserViewSet(viewsets.ModelViewSet):
-    permission_class = [IsAdminUser]
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class UserList(generics.ListCreateAPIView):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = "posts/post_list.html"
+
+    paginate_by = 5
